e305e982b13ead05df12d3fb0d16311301feb14d
amitpaultl <amitpaultl@gmail.com>
2022-10-25 05:56:08 +0600

    Server is ring

--- index.js
+++ index.js
@@ -4,11 +4,24 @@
  app.use(cors());
 
  const Port = process.env.Port || 5000;
-
+ const courses = require('./data/courses.json')
  app.get('/',(req, res)=>{
     res.send('5000 port rining')
+    console.log(res);
  })
 
+
+ app.get('/course', (req, res) =>{
+   res.send(courses)
+   
+})
+
+app.get('/course/:id', (req, res) =>{
+   const id = parseInt(req.params.id);
+   const course = courses.find(number => number.id === id);
+   res.send(course);
+
+})
 
  app.listen(Port, ()=>{
     console.log('your server ring');
